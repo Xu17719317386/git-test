4a0bfe095ffc858b1b319cdd9e4d8a2ba70d1110
XugJin <2829135184@qq.com>
2020-06-30 19:03:55 +0800

    向本地仓库提交文件

---
+++ glit-test-project/src/cn/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package cn.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+    }
+}
